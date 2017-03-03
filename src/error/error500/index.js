692e0411f375c299a93b6ed3c318748136a8e735
E104TW\kidd.fan <kidd.fan@104.com.tw>
2017-03-03 15:39:26 +0800

    fix code

--- src/error/error500/index.js
+++ src/error/error500/index.js
@@ -1,6 +1,6 @@
 module.exports  = (err, req, res, next) => {
 	if(err){
-	   res.status(200);
+	   res.status(500);
 	   var jsonInfo = {"code":"500", "detail":"Internal Server Error"}
 	   res.json(jsonInfo);
 	}
